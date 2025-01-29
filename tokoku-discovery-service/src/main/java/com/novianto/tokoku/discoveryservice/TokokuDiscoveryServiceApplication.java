8623b0385c5d42682f948c8e85dcc81f7a67cc03
noppiii <mnoviantoanggoro@gmail.com>
2025-01-29 19:47:47 +0700

    setup discovery server

--- tokoku-discovery-service/src/main/java/com/novianto/tokoku/discoveryservice/TokokuDiscoveryServiceApplication.java
+++ tokoku-discovery-service/src/main/java/com/novianto/tokoku/discoveryservice/TokokuDiscoveryServiceApplication.java
@@ -2,11 +2,16 @@ package com.novianto.tokoku.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class TokokuDiscoveryServiceApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("GMT +0:00"));
 		SpringApplication.run(TokokuDiscoveryServiceApplication.class, args);
 	}
 
